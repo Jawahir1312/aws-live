1e005be919740f4e856c059ef9b0e3ab2c32430a
Jawahir1312 <96032977+Jawahir1312@users.noreply.github.com>
2024-09-01 19:10:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cywjqgrpgjq4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jawahir-employee"
+customregion = "us-east-1"
+
